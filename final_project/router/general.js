3ef88398fb89a731ee3bde1ef6b5a0ff333ceebc
Sareuth <sareuth@gmail.com>
2026-06-06 05:27:20 -0400

    Task 5 Completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -55,8 +55,16 @@ public_users.get('/title/:title', function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
-    //Write your code here
-    return res.status(300).json({ message: "Yet to be implemented" });
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    if (book) {
+        const reviews = book.reviews;
+        if (Object.keys(reviews).length > 0)
+            return res.status(200).send(JSON.stringify(reviews,null,4));
+        else
+            return res.status(200).json({message: "No reviews found for this book."});
+    } else
+        return res.status(404).json({ message: "Book not found"});
 });
 
 module.exports.general = public_users;
